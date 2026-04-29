698b60d750374249f7f65f4ef1b6f0e27613c557
Sarinyatee <sarinyatee@gmail.com>
2026-04-28 23:58:53 -0700

    Add isValid and isSolved in SudokuBoard
    - isValid: return true if all of the following constraints are followed according to the board's current data; false otherwise:
    there is no data in the board that is not a 1-9 or a space (or your representation of a space - could be a '.' or a 0, etc)
    no row contains any duplicate values of 1-9
    no column contains any duplicate values of 1-9
    no mini-square contains any duplicate values of 1-9
    
    - isSolved: return true if there are nine occurrences of every number 1-9 in the grid AND all the constraints of isValid are followed. This method use Map to study its syntax and how to use.

--- SudokuBoard.java
+++ SudokuBoard.java
@@ -4,16 +4,15 @@ import java.io.*;
 
 public class SudokuBoard {
    private char[][] board;
-   private Set<Char> set = new HashSet<Char>();
    
    public SudokuBoard(String file){
       board = new char[9][9];
       try {
          Scanner console = new Scanner(new File(file));
-         for(int r = 0; r < 9; r++) {
+         for(int r = 0; r < board.length; r++) {
             if(console.hasNextLine()){
                String line = console.nextLine();
-               for(int c = 0; c < 9; c++) {
+               for(int c = 0; c < board[0].length; c++) {
                   board[r][c] = line.charAt(c);
                }
             }
@@ -24,71 +23,123 @@ public class SudokuBoard {
       }
    }
    
-   public boolean isValid(char[][] board) {
-      if(noData() == true){ return false; }
-      else if {checkRow == true)
-      
-      
-      checkColumn();
-      
-      
-
+   public boolean isValid() {
+      return checkData() && checkRow() && checkColumn() && checkMiniSquare();
    }
    
-   private boolean checkRow(char[][] board) {
-      Set<Charcater> set = new HashSet<Character>();
-      //check row
-      for(int r = 0; r < board[r].length; r++) {
-         for(int c = 0; c < length[0].length; c++) {
+   private boolean checkData() {
+      Set<Character> set = new HashSet<Character>();
+      //create valid set of 1-9, space and .
+      for(int i = 1; i <= 9; i++) {
+         char j = (char)(i + '0');
+         set.add(j);
+      }
+      set.add(' ');
+      set.add('.');
+      for(int r = 0; r < board.length; r++) {
+         for(int c = 0; c < board[r].length; c++) {
+            if(!set.contains(board[r][c])) {
+               return false;
+            }
+         }
+      }
+      return true;  
+   }
+   
+   private boolean checkRow() {
+      for(int r = 0; r < board.length; r++) {
+         Set<Character> set = new HashSet<Character>();
+         for(int c = 0; c < board[r].length; c++) {
            if(set.contains(board[r][c]) && board[r][c] != '.') {
-            return false;
+               return false;
             } else {
                set.add(board[r][c]);            
             }
+         }
        }
+       return true;
    }
    
-   private boolean checkColumn(char[][] board) {
-      Set<Char> set = new HashSet<Char>();
-      //check column
-      for(int c = 0; c < length[0].length; c++) {
-         for(int r = 0; r < board[r].length; r++) {
+   private boolean checkColumn() {
+      for(int c = 0; c < board[0].length; c++) {
+         Set<Character> set = new HashSet<Character>();
+         for(int r = 0; r < board.length; r++) {
            if(set.contains(board[r][c]) && board[r][c] != '.') {
-            return false;
+               return false;
             } else {
                set.add(board[r][c]);            
-            }
+              }
+         }
        }
+       return true;
    }
    
-   private int[][] miniSquare(int spot) {
-      int[][] mini = new int[3][3];
+   private char[][] miniSquare(int spot) {
+      char[][] mini = new char[3][3];
       for(int r = 0; r < 3; r++) {
-         for(int c = 0; c <3; c++) {
+         for(int c = 0; c < 3; c++) {
             mini[r][c] = board[(spot-1) / 3 * 3 + r][(spot - 1) % 3 * 3 + c];
          }   
       }
       return mini; 
    }
-   private boolean checkSquare(char[][] board) {
-       Set<Char> set = new HashSet<Char>();
-       // There is 9 spots
-       for( int i = 1; i <= 9; i++) {
-         miniSquare(i);
-           
-       }
-
+   private boolean checkMiniSquare() {
+      for(int spot = 1; spot <= 9; spot++) {
+         Set<Character> set = new HashSet<Character>();
+         char[][] mini = miniSquare(spot);
+         for(int r = 0; r < 3; r++) {
+            for(int c = 0; c < 3; c++) {
+               if(set.contains(mini[r][c]) && mini[r][c] != '.') {
+                  return false;
+               } else {
+                  set.add(mini[r][c]);            
+              }
+            }
+         }
+     }        
+      return true;
    }
-    
+   
+   public boolean isSolved() {  
+      if (!isValid()) {
+        return false;
+      }
+      Map<Character, Integer> counts = new HashMap<>();
+      for (int r = 0; r < board.length; r++) {
+        for (int c = 0; c < board[0].length; c++) {
+            char val = board[r][c];
+            int num = val - '0';          
+            if (num >= 1 && num <= 9) {
+                if (counts.containsKey(val)) {
+                    counts.put(val, counts.get(val) + 1);
+                } else {
+                    counts.put(val, 1);
+                }
+            }
+        }
+    }
+    return checkMap(counts);
+   }
+
+   private boolean checkMap(Map<Character, Integer> map) {
+      for(int i = 1; i <= 9; i++) {
+         char result = (char)(i + '0');
+         if (!map.containsKey(result) || map.get(result) != 9) {
+            return false;
+        }
+      }
+      return true;
+   }
+   
    public String toString() {
       String divider = "----------------------";
       StringBuilder make = new StringBuilder();
       
-      for(int r = 0; r < 9; r++) {
+      for(int r = 0; r < board.length; r++) {
          if(r % 3 == 0) {
             make.append(divider).append("\n");
          }
-         for(int c = 0; c < 9; c++) {
+         for(int c = 0; c < board[0].length; c++) {
             if(c % 3 == 0) {
                make.append("|");
             }
@@ -99,22 +150,31 @@ public class SudokuBoard {
       make.append(divider);
       return make.toString();
    }
-   
+}   
+/* Change log
+SudokuChecker.java
+------------------Topic------------------|------------------Detail------------------
+1. Add  import java.util.*,              |
+       import java.io.*;                 |
+2. Change MySudokuBoard to               | It should be the same name as OOP class.
+   SudokuBoard
 
-   
- //     SudokuBoard board = new SudokuBoard("data1.sdk");
- //     System.out.println(board);
-}
-/*----------------------
- |2 . . |1 . 5 |. . 3 |
- |. 5 4 |. . . |7 1 . |
- |. 1 . |2 . 3 |. 8 . |
- ----------------------
- |6 . 2 |8 . 7 |3 . 4 |
- |. . . |. . . |. . . |
- |1 . 5 |3 . 9 |8 . 6 |
- ----------------------
- |. 2 . |7 . 1 |. 6 . |
- |. 8 1 |. . . |2 4 . |
- |7 . . |4 . 2 |. . 1 |
- ----------------------
+SudokuBoard.java
+---------------------Topic---------------------|------------------Detail------------------
+1. A position of                               |- Change a position from after checkRow(), checkColumn() and checkMiniSquare()
+Set<Character> set = new HashSet<Character>(); |to in the first for loop of method because I want to use new set every end of
+                                               |the first loop.
+2. private int[][] miniSquare(int spot),       |- Change int[][] to char[][] to the same data type as private char[][] board;                                        
+   int[][] mini = new int[3][3];               | 
+
+/* Program Output
+ Checking empty board...passed.
+ Checking incomplete, valid board...passed.
+ Checking complete, valid board...passed.
+ Checking dirty data board...passed.
+ Checking row violating board...passed.
+ Checking col violating board...passed.
+ Checking row&col violating board...passed.
+ Checking mini-square violating board...passed.
+ **** HORRAY: ALL TESTS PASSED ****
+*/
