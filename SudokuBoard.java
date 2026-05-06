dc02e3b55dfe9b30b2437589890c38dfde2c1a38
Sarinyatee <sarinyatee@gmail.com>
2026-05-06 12:20:18 -0700

    Add solve() in SudokuBoard.java
    Part 1: solve() method header and initial checks
    - If your board state is invalid
    - If your board is already solved
    - In all other cases you have an incomplete
      - If in your exhaustive searching you find a solution, you'll end up returning true; but
      - If you never find a solution, you'll end up returning false.
    Part 2: solve() with recursive backtracking

--- SudokuBoard.java
+++ SudokuBoard.java
@@ -1,180 +1,197 @@
 import java.util.*;
 import java.io.*;
 
-
 public class SudokuBoard {
-   private char[][] board;
-   
-   public SudokuBoard(String file){
-      board = new char[9][9];
-      try {
-         Scanner console = new Scanner(new File(file));
-         for(int r = 0; r < board.length; r++) {
-            if(console.hasNextLine()){
-               String line = console.nextLine();
-               for(int c = 0; c < board[0].length; c++) {
-                  board[r][c] = line.charAt(c);
-               }
+    private char[][] board;
+
+    public SudokuBoard(String path) {
+        board = new char[9][9];
+        loadFile(path);
+    }
+
+    private void loadFile(String path) {
+        try {
+            Scanner scan = new Scanner(new File(path));
+            for (int r = 0; r < board.length; r++) {
+                String line = scan.nextLine();
+                for (int c = 0; c < board[r].length; c++) {
+                    if (line.charAt(c) == '.') {
+                        board[r][c] = '-';
+                    } else {
+                        board[r][c] = line.charAt(c);
+                    }
+                }
             }
-         }
-         console.close();
-      } catch(FileNotFoundException e) {
-         System.out.println("File not found!");
-      }
-   }
-   
-   public boolean isValid() {
-      return checkData() && checkRow() && checkColumn() && checkMiniSquare();
-   }
-   
-   private boolean checkData() {
-      Set<Character> set = new HashSet<Character>();
-      //create valid set of 1-9, space and .
-      for(int i = 1; i <= 9; i++) {
-         char j = (char)(i + '0');
-         set.add(j);
-      }
-      set.add(' ');
-      set.add('.');
-      for(int r = 0; r < board.length; r++) {
-         for(int c = 0; c < board[r].length; c++) {
-            if(!set.contains(board[r][c])) {
-               return false;
-            }
-         }
-      }
-      return true;  
-   }
-   
-   private boolean checkRow() {
-      for(int r = 0; r < board.length; r++) {
-         Set<Character> set = new HashSet<Character>();
-         for(int c = 0; c < board[r].length; c++) {
-           if(set.contains(board[r][c]) && board[r][c] != '.') {
-               return false;
-            } else {
-               set.add(board[r][c]);            
-            }
-         }
-       }
-       return true;
-   }
-   
-   private boolean checkColumn() {
-      for(int c = 0; c < board[0].length; c++) {
-         Set<Character> set = new HashSet<Character>();
-         for(int r = 0; r < board.length; r++) {
-           if(set.contains(board[r][c]) && board[r][c] != '.') {
-               return false;
-            } else {
-               set.add(board[r][c]);            
-              }
-         }
-       }
-       return true;
-   }
-   
-   private char[][] miniSquare(int spot) {
-      char[][] mini = new char[3][3];
-      for(int r = 0; r < 3; r++) {
-         for(int c = 0; c < 3; c++) {
-            mini[r][c] = board[(spot-1) / 3 * 3 + r][(spot - 1) % 3 * 3 + c];
-         }   
-      }
-      return mini; 
-   }
-   private boolean checkMiniSquare() {
-      for(int spot = 1; spot <= 9; spot++) {
-         Set<Character> set = new HashSet<Character>();
-         char[][] mini = miniSquare(spot);
-         for(int r = 0; r < 3; r++) {
-            for(int c = 0; c < 3; c++) {
-               if(set.contains(mini[r][c]) && mini[r][c] != '.') {
-                  return false;
-               } else {
-                  set.add(mini[r][c]);            
-              }
-            }
-         }
-     }        
-      return true;
-   }
-   
-   public boolean isSolved() {  
-      if (!isValid()) {
-        return false;
-      }
-      Map<Character, Integer> counts = new HashMap<>();
-      for (int r = 0; r < board.length; r++) {
-        for (int c = 0; c < board[0].length; c++) {
-            char val = board[r][c];
-            int num = val - '0';          
-            if (num >= 1 && num <= 9) {
-                if (counts.containsKey(val)) {
-                    counts.put(val, counts.get(val) + 1);
-                } else {
-                    counts.put(val, 1);
+        } catch (FileNotFoundException e) {
+            System.out.println("Failed to load from file: " + e
+            .getMessage());
+        }
+    }
+
+    public boolean isValid() {
+        return checkCharacter() && checkRowDuplicate() &&
+            checkColumnDuplicate() && checkMiniSquareDuplicate();
+    }
+
+    private boolean checkCharacter() {
+        for (int r = 0; r < board.length; r++) {
+            for (int c = 0; c < board[r].length; c++) {
+                // Each character on the board must be a number, or a dash
+                if (board[r][c] < '0' || board[r][c] > '9') {
+                    // The only acceptable value other than a number is a dash
+                    if (board[r][c] != '-') {
+                        return false;
+                    }
                 }
             }
         }
+        return true;
     }
-    return checkMap(counts);
-   }
 
-   private boolean checkMap(Map<Character, Integer> map) {
-      for(int i = 1; i <= 9; i++) {
-         char result = (char)(i + '0');
-         if (!map.containsKey(result) || map.get(result) != 9) {
+    private boolean checkRowDuplicate() {
+        // Row major iteration
+        for (int r = 0; r < board.length; r++) {
+            // Uses a set to store values that we have seen
+            Set < Character > rowValue = new HashSet < > ();
+            for (int c = 0; c < board[r].length; c++) {
+                if (rowValue.contains(board[r][c]) && board[r][c] != '-') {
+                    return false;
+                }
+                rowValue.add(board[r][c]);
+            }
+        }
+        return true;
+    }
+
+    private boolean checkColumnDuplicate() {
+        // Column major iteration
+        for (int c = 0; c < board.length; c++) {
+            // Uses a set to store values that we have seen
+            Set < Character > colValue = new HashSet < > ();
+            for (int r = 0; r < board[c].length; r++) {
+                if (colValue.contains(board[r][c]) && board[r][c] != '-') {
+                    return false;
+                }
+                colValue.add(board[r][c]);
+            }
+        }
+        return true;
+    }
+
+    private char[][] miniSquare(int spot) {
+        char[][] mini = new char[3][3];
+        for (int r = 0; r < 3; r++) {
+            for (int c = 0; c < 3; c++) {
+                // whoa - wild! This took me a solid hour to figure out (at least)
+                // This translates between the "spot" in the 9x9 Sudoku board
+                // and a new mini square of 3x3
+                mini[r][c] = board[(spot - 1) / 3 * 3 + r][(spot - 1) % 3 *
+                    3 + c
+                ];
+            }
+        }
+        return mini;
+    }
+
+    private boolean checkMiniSquareDuplicate() {
+        // Iterates through every mini square
+        for (int spot = 1; spot <= 9; spot++) {
+            char[][] miniSquare = miniSquare(spot);
+
+            // Uses a set to store values that we have seen
+            Set < Character > gridValue = new HashSet < > ();
+
+            // Row major iteration for a mini square rather than the entire board
+            for (int r = 0; r < miniSquare.length; r++) {
+                for (int c = 0; c < miniSquare[r].length; c++) {
+                    if (gridValue.contains(miniSquare[r][c]) && miniSquare[
+                            r][c] != '-') {
+                        return false;
+                    }
+                    gridValue.add(miniSquare[r][c]);
+                }
+            }
+        }
+        return true;
+    }
+
+    public boolean isSolved() {
+        if (!isValid()) {
             return false;
         }
-      }
-      return true;
-   }
-   
-   public String toString() {
-      String divider = "----------------------";
-      StringBuilder make = new StringBuilder();
-      
-      for(int r = 0; r < board.length; r++) {
-         if(r % 3 == 0) {
-            make.append(divider).append("\n");
-         }
-         for(int c = 0; c < board[0].length; c++) {
-            if(c % 3 == 0) {
-               make.append("|");
+
+        Map < Character, Integer > boardMap = new HashMap < > ();
+        for (int r = 0; r < board.length; r++) {
+            for (int c = 0; c < board[r].length; c++) {
+                if (!boardMap.containsKey(board[r][c])) {
+                    // Create new key in boardMap after seeing a number for the first time
+                    boardMap.put(board[r][c], 1);
+                } else {
+                    // Increment the value if the number already exists on the board
+                    boardMap.put(board[r][c], boardMap.get(board[r][c]) +
+                    1);
+                }
             }
-            make.append(board[r][c]).append(" ");
+        }
+
+        // Check whether every value in the boardMap is 9, meaning 9 occurrences of each number
+        for (char key: boardMap.keySet()) {
+            // A dash is a legal character in boardMap but shouldn't ever exist in a solved board
+            if (key == '-') {
+                return false;
+            }
+            if (boardMap.get(key) != 9) {
+                return false;
+            }
+        }
+        return true;
+    }
+    
+    public boolean solve() {
+      if (!isValid()) {
+          return false;
+        } 
+      if(isSolved()) {
+         return true;
+      } else {
+         for(int c = 0; c < board[0].length; c++) {
+            for(int r = 0; r < board.length; r++) {
+               if(board[c][r] == '-') {
+                  for(char num = '1'; num <= '9'; num++){
+                     board[c][r] = num;
+                     if(solve()) {
+                        return true;
+                     }
+                     //board[c][r] = '-';
+                  }
+                  //return false;
+               }
+            }       
          }
-         make.append("|\n");
-      }
-      make.append(divider);
-      return make.toString();
-   }
-}   
-/* Change log
-SudokuChecker.java
-------------------Topic------------------|------------------Detail------------------
-1. Add  import java.util.*,              |
-       import java.io.*;                 |
-2. Change MySudokuBoard to               | It should be the same name as OOP class.
-   SudokuBoard
-
-SudokuBoard.java
----------------------Topic---------------------|------------------Detail------------------
-1. A position of                               |- Change a position from after checkRow(), checkColumn() and checkMiniSquare()
-Set<Character> set = new HashSet<Character>(); |to in the first for loop of method because I want to use new set every end of
-                                               |the first loop.
-2. private int[][] miniSquare(int spot),       |- Change int[][] to char[][] to the same data type as private char[][] board;                                        
-   int[][] mini = new int[3][3];               | 
-
-/* Program Output
- Checking empty board...passed.
- Checking incomplete, valid board...passed.
- Checking complete, valid board...passed.
- Checking dirty data board...passed.
- Checking row violating board...passed.
- Checking col violating board...passed.
- Checking row&col violating board...passed.
- Checking mini-square violating board...passed.
- **** HORRAY: ALL TESTS PASSED ****
-*/
+      return false;
+    }
+    }
+    
+    public String toString() {
+        StringBuilder result = new StringBuilder();
+        for (int r = 0; r < board.length - 1; r++) {
+            result.append(" ");
+            result.append(board[r][0]);
+            for (int c = 1; c < board.length; c++) {
+                result.append(" | ");
+                result.append(board[r][c]);
+            }
+            result.append("\n");
+            result.append("–––");
+            result.append("|–––".repeat(8));
+            result.append("\n");
+        }
+        result.append(" ");
+        result.append(board[board.length - 1][0]);
+        for (int c = 1; c < board.length; c++) {
+            result.append(" | ");
+            result.append(board[board.length - 1][c]);
+        }
+        return result.toString();
+    }
+}
